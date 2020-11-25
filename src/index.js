877b26b435125d803e2eb1a29b8f5302b946c951
Pavel <pl.hancharou@gmail.com>
2020-11-25 18:29:41 +0300

    fix: add polyfills

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
 'use strict';
 
+import 'nodelist-foreach-polyfill';
+import '@babel/polyfill';
+import elementClosest from 'element-closest';
+elementClosest(window);
+import 'formdata-polyfill';
+import 'es6-promise';
+import 'fetch-polyfill';
+import 'regexp-polyfill';
+import browser from 'cross-browser-polyfill';
+browser();
+
 import dropDownMenu from './modules/dropDownMenu';
 
 //Drop-down menu
