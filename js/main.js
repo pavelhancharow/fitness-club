caab1c40851b1394d67a13cef9606ccd9c265ebf
Pavel <pl.hancharou@gmail.com>
2020-11-25 15:59:02 +0300

    feat: create dropDownMenu()

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 window.addEventListener('DOMContentLoaded', function () {
   'use strict';
 
+  //Drop-down menu
+  const dropDownMenu = () => {
+    const clubList = document.querySelector('.clubs-list > ul');
+
+    document.addEventListener('click', (e) => {
+      clubList.style.display = (e.target.closest('.club-select')) ? 'block' : 'none';
+    });
+  };
+  dropDownMenu();
+
+
 });
