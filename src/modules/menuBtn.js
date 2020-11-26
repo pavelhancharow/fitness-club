80eaeed338c7d03dca0d887253982bdae597b4a4
Pavel <pl.hancharou@gmail.com>
2020-11-26 15:04:10 +0300

    feat: create menuBtn()

---
+++ src/modules/menuBtn.js
@@ -0,0 +1,32 @@
+const menuBtn = () => {
+  const topMenu = document.querySelector('.top-menu'),
+    headSlider = document.querySelector('.head-slider'),
+    headWrap = document.querySelector('.head .wrapper'),
+    popupMenu = document.querySelector('.popup-menu'),
+    hiddenSmall = document.querySelector('.hidden-small'),
+    hiddenLarge = document.querySelector('.hidden-large');
+
+  if (screen.width < 768) {
+    hiddenSmall.style.display = 'none';
+    hiddenLarge.style.display = 'block';
+
+    topMenu.style.top = `${headWrap.clientHeight}px`;
+    topMenu.style.position = 'fixed';
+    headSlider.style.paddingTop = `${topMenu.clientHeight}px`;
+
+    window.addEventListener('scroll', () => {
+      topMenu.style.top = (window.scrollY < headWrap.clientHeight) ? `${headWrap.clientHeight - window.scrollY}px` : `0px`;
+    });
+
+    document.addEventListener('click', (e) => {
+      if (e.target.matches('.menu-button img')) {
+        popupMenu.style.display = 'flex';
+      } else if (e.target.matches('.close-menu-btn img') || e.target.matches('.scroll a')) {
+        popupMenu.style.display = 'none';
+      }
+    });
+  }
+
+};
+
+export default menuBtn;
