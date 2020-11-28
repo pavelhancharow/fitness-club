26746cafdd5271db3f31e211cd2f6bd649ea4db8
Pavel <pl.hancharou@gmail.com>
2020-11-28 13:13:26 +0300

    fix: add Event Listener resize

--- src/modules/menuBtn.js
+++ src/modules/menuBtn.js
@@ -6,27 +6,34 @@ const menuBtn = () => {
     hiddenSmall = document.querySelector('.hidden-small'),
     hiddenLarge = document.querySelector('.hidden-large');
 
-  if (document.documentElement.clientWidth < 768) {
-    hiddenSmall.style.display = 'none';
-    hiddenLarge.style.display = 'block';
+  const resizeScreen = () => {
+    if (document.documentElement.clientWidth < 768) {
+      hiddenSmall.style.display = 'none';
+      hiddenLarge.style.display = 'block';
 
-    topMenu.style.top = `${headWrap.clientHeight}px`;
-    topMenu.style.position = 'fixed';
-    headSlider.style.paddingTop = `${topMenu.clientHeight}px`;
+      topMenu.style.top = `${headWrap.clientHeight}px`;
+      topMenu.style.position = 'fixed';
+      headSlider.style.paddingTop = `${topMenu.clientHeight}px`;
 
-    window.addEventListener('scroll', () => {
-      topMenu.style.top = (window.scrollY < headWrap.clientHeight) ? `${headWrap.clientHeight - window.scrollY}px` : `0px`;
-    });
+      window.addEventListener('scroll', () => {
+        topMenu.style.top = (window.scrollY < headWrap.clientHeight) ? `${headWrap.clientHeight - window.scrollY}px` : `0px`;
+      });
 
-    document.addEventListener('click', (e) => {
-      if (e.target.matches('.menu-button img')) {
-        popupMenu.style.display = 'flex';
-      } else if (e.target.matches('.close-menu-btn img') || e.target.matches('.scroll a')) {
-        popupMenu.style.display = 'none';
-      }
-    });
-  }
+      document.addEventListener('click', (e) => {
+        if (e.target.matches('.menu-button img')) {
+          popupMenu.style.display = 'flex';
+        } else if (e.target.matches('.close-menu-btn img') || e.target.matches('.scroll a')) {
+          popupMenu.style.display = 'none';
+        }
+      });
+    } else {
+      hiddenSmall.style.display = 'flex';
+      hiddenLarge.style.display = 'none';
+    }
+  };
+  resizeScreen();
 
+  window.addEventListener('resize', () => resizeScreen());
 };
 
 export default menuBtn;
