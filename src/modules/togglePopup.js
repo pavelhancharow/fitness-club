99e88f00847bd8f9ea11cd4ea526bd247634e722
Pavel <pl.hancharou@gmail.com>
2020-11-28 20:40:31 +0300

    fix: add clear inputs if popup close

--- src/modules/togglePopup.js
+++ src/modules/togglePopup.js
@@ -1,7 +1,8 @@
 const togglePopup = () => {
   const popupToggle = document.querySelectorAll('.open-popup, .callback-btn, .fixed-gift, .popup'),
     giftPopup = document.getElementById('gift'),
-    forms = document.querySelectorAll('#form2, #form1');
+    forms = document.querySelectorAll('#form2, #form1'),
+    thanks = document.getElementById('thanks');
 
   popupToggle.forEach(item => {
     item.addEventListener('click', (e) => {
@@ -15,18 +16,22 @@ const togglePopup = () => {
         giftPopup.style.display = 'block';
         e.target.style.display = 'none';
       } else if (e.target.matches('.close_icon') || !e.target.closest('.form-content') || e.target.matches('.close-btn')) {
-        item.style.display = 'none';
-        // forms.forEach(form => {
-        //   const inputs = form.querySelectorAll('input');
-        //   inputs.forEach(input => {
-        //     if (input.type === 'checkbox') {
-        //       input.checked = false;
-        //     }
-        //     if (input.type === 'tel' || input.name === 'name') {
-        //       input.value = '';
-        //     }
-        //   });
-        // });
+        if (thanks.style.display !== 'block') {
+          forms.forEach(form => {
+            const inputs = form.querySelectorAll('input');
+            inputs.forEach(input => {
+              if (input.type === 'checkbox') {
+                input.checked = false;
+              }
+              if (input.type === 'tel' || input.name === 'name') {
+                input.value = '';
+              }
+            });
+          });
+          item.style.display = 'none';
+        } else {
+          item.style.display = 'none';
+        }
       }
     });
   });
